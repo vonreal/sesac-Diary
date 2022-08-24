8105ddccf8c5ae5155e52f905f99bae0d376b1c6
jiun <vonreal20@gmail.com>
2022-08-24 09:27:34 +0900

    🌱 [0824 - 수업] : transition Extension 실습

---
+++ sesac-Diary/Extension/Transition+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Transition+Extension.swift
+//  sesac-Diary
+//
+//  Created by 나지운 on 2022/08/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    enum TransitionStyle {
+        case present
+        case presentNavigation
+        case presentFullNavigation
+        case push
+    }
+    
+    func transition<T: UIViewController>(_ viewController: T, transitionStyle: TransitionStyle = .present) {
+        switch transitionStyle {
+        case .present:
+            self.present(viewController, animated: true)
+        case .presentNavigation:
+            let nav = UINavigationController(rootViewController: viewController)
+            self.present(nav, animated: true)
+        case .presentFullNavigation:
+            let nav = UINavigationController(rootViewController: viewController)
+            nav.modalPresentationStyle = .overFullScreen
+            self.present(nav, animated: true)
+        case .push:
+            self.navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+}
